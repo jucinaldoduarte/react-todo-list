04de59d2a3d7c2ff875c171073358f41cb10aa5d
jucinaldo.duarte <jucinaldo.duarte@gmail.com>
2025-05-07 20:15:44 -0300

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,34 @@
-//30:37
-
-/* import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-
-
-export default App
-
-*/
-
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 import "./styles.css"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([])
-  
-  function handleSubmit(e) {
-    e.preventDefault()
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
+    return JSON.parse(localValue)
+})
+
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS,", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
     setTodos((currentTodos) => {
       return [
         ...currentTodos, 
-      { id: crypto.randomUUID(), title: newItem, completed: 
-        false }
+      { id: crypto.randomUUID(), title, completed: false }
       ]
-    })  
-
-    setNewItem("")
+    }) 
   }
 
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
-        if (todo.id == id) {
-          //todo.completed = completed
+        if (todo.id === id) {
           return { ...todo, completed}
         }
         return todo
@@ -76,50 +39,14 @@ export default function App() {
   function deleteTodo(id) {
     setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
-    }
-
-    )
+    })
   }
 
-
- // console.log(todos)
-
   return (
-  <> 
-    <form onSubmit={handleSubmit} className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item </label>
-        <input 
-          value={newItem} 
-          onChange={e => setNewItem(e.target.value)}
-          type="text" 
-          id='item'
-        />        
-      </div>
-      <button className="btn">Add</button>
-    </form>
-    <h1 className="header">Todo List</h1>  
-    <ul className="list">
-      {todos.length === 0 && "No Todos"}
-      {todos.map(todo => {
-        return (
-          <li key={todo.id}>
-            <label>
-              <input 
-                type="checkbox" 
-                checked={todo.completed} 
-                onChange={e => toggleTodo(todo.id, e.target.checked)} 
-              />
-              {todo.title}
-            </label>
-            <button onClick={() => deleteTodo(todo.id)} 
-              className="btn btn-danger">
-                Delete
-            </button>
-          </li>
-      )
-      })}      
-    </ul>
+    <> 
+      <NewTodoForm onSubmit={addTodo}/>
+      <h1 className="header">Todo List</h1>  
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
     </>    
   )
 }
