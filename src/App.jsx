482feaa681a6ce2414c881bed9244939fc377271
jucinaldo.duarte <jucinaldo.duarte@gmail.com>
2025-05-07 14:56:22 -0300

    new file:   .gitignore new file:   README.md new file:   eslint.config.js new file:   index.html new file:   package-lock.json new file:   package.json new file:   public/vite.svg new file:   src/App.css new file:   src/App.jsx new file:   src/assets/react.svg new file:   src/index.css new file:   src/main.jsx new file:   src/styles.css deleted:    test.txt new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,125 @@
+//30:37
+
+/* import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+
+
+export default App
+
+*/
+
+import { useState } from "react"
+import "./styles.css"
+
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+  
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos, 
+      { id: crypto.randomUUID(), title: newItem, completed: 
+        false }
+      ]
+    })  
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id == id) {
+          //todo.completed = completed
+          return { ...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    }
+
+    )
+  }
+
+
+ // console.log(todos)
+
+  return (
+  <> 
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item </label>
+        <input 
+          value={newItem} 
+          onChange={e => setNewItem(e.target.value)}
+          type="text" 
+          id='item'
+        />        
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>  
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label>
+              <input 
+                type="checkbox" 
+                checked={todo.completed} 
+                onChange={e => toggleTodo(todo.id, e.target.checked)} 
+              />
+              {todo.title}
+            </label>
+            <button onClick={() => deleteTodo(todo.id)} 
+              className="btn btn-danger">
+                Delete
+            </button>
+          </li>
+      )
+      })}      
+    </ul>
+    </>    
+  )
+}
